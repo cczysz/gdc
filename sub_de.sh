73cfdaf09a4f3fd59804ffd67d19951bc6a95deb
Charles Czysz <charles.czysz@gmail.com>
2016-04-26 16:39:51 -0500

    subscript for de script

---
+++ sub_de.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#PBS -N czysz_tcga
+#PBS -S /bin/bash
+#PBS -l walltime=30:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=32gb
+
+#PBS -o $HOME/kirp.out
+#PBS -e $HOME/kirp.err
+
+module load R/3.2.0
+
+#export TEMPDIR=/home/t.cri.cczysz/tmp
+Rscript /home/t.cri.cczysz/gdc/count_DE.R
