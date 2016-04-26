9fde56848749e05894c6c4b77d2ea6a91c8cf625
Charles Czysz <charles.czysz@gmail.com>
2016-04-26 09:35:09 -0500

    removing duplicate individuals

--- expr.R
+++ expr.R
@@ -3,13 +3,20 @@
 phens <- read.table(file='phens.txt', sep='\t', header=T)
 
 files_in <- read.table(file='files.txt', header=F)
-files_in <- files_in[!(duplicated(files_in$V4)),]
-q()
+dup_ind <- read.table(file='duplicated.txt', header=F)
+omit <- (files_in$V4%in%dup_ind$V1)
+
+files_in <- files_in[!omit, ]
+
+phens_and_expr <- intersect(phens$bcr_patient_uuid, files_in$V4)
+phens <- phens[phens$bcr_patient_uuid%in%phens_and_expr,]
+# files_in <- files_in[!(duplicated(files_in$V4)),]
 # Recreate phenotype and expression objects
 idx <- match(files_in[,4], phens[,2])
 idx2 <- match(phens[,2], files_in[,4])
 phens <- phens[idx,]
 phens$File <- files_in$V1
+
 df_list <- lapply(phens$File, read.table, header=F, row.names=1)
 
 expr_df <- data.frame(row.names=rownames(df_list[[1]]))
@@ -17,3 +24,5 @@ for (i in seq(length(df_list))) {
 	expr_df <- data.frame(expr_df, df_list[[i]])
 }
 rm(df_list)
+colnames(expr_df) <- phens[,1]
+cond <- data.frame(row.names=phens[,1], Sex=factor(phens$gender))
